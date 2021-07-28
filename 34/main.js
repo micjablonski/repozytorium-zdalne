2133a56928089b3ed67349f480ce8cd754a12de8
mjablonski <jablonski.mn@gmail.com>
2021-07-28 23:56:26 +0200

    Plik bazowy

---
+++ 34/main.js
@@ -0,0 +1,6 @@
+let zmienna1 = "Uwielbiam JavaScript";
+let zmienna2 = "Jestem świetnym programistą";
+function wypisz(zmienna1, zmienna2) {
+    return zmienna1 + ' i ' + zmienna2;
+}
+console.log(wypisz(zmienna1, zmienna2));
