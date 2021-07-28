aff464b71f174651d38fcd87735df28c9f33e90a
mjablonski <jablonski.mn@gmail.com>
2021-07-28 14:04:21 +0200

    Plik bazowy

---
+++ 29/js/main.js
@@ -0,0 +1,7 @@
+let firstNumber = 10;
+let secondNumber = 12;
+function obliczenia(firstNumber, secondNumber) {
+    let suma = firstNumber + secondNumber;
+    return suma;
+}
+console.log(obliczenia(firstNumber, secondNumber));
